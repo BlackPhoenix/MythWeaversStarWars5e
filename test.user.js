721ba43a92bb6fce44ca4264898708b4ca6b61aa
BlackPhoenix <demerss@gmail.com>
2021-08-05 11:28:31 -0400

    Create test.user.js

---
+++ test.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Test Script
+// @namespace    http://tampermonkey.net/
+// @version      0.19
+// @description  To test changing the homepage / updateURL
+// @author       BlackPhoenix
+// @match        https://www.myth-weavers.com/sheet.html
+// @grant        none
+// @supportURL   https://github.com/BlackPhoenix/MythWeaversStarWars5e/issues
+// @homepageURL  https://github.com/BlackPhoenix/MythWeaversStarWars5e
+// ==/UserScript==
+
+'use strict';
